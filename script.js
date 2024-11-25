69668433648fd9c73fa93251a48525933107b19c
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-11-25 15:02:51 +0500

    .

---
+++ script.js
@@ -0,0 +1,205 @@
+//stringga oid
+//1-masala
+// let text = "teskari";
+// let res = text
+//   .split(" ")
+//   .map(function (value) {
+//     return value.split("").reverse().join("");
+//   })
+//   .join(" ");
+// console.log(res);
+
+
+//2-masala
+// let text = 'salom';
+// let res = text.split('').reverse().join('');
+// console.log(res);
+
+
+
+//3-masala
+// let text = 'Berilgan sozda nechta Bosh Harf Bor';
+// let res = text.split('');
+// let sum = res.filter(value => value === value.toUpperCase());
+// console.log(sum);
+
+
+//6-masala
+// let text = 'test'
+// let res = text.length
+// console.log(res);
+
+//8-masala
+// let text = 'hello'
+// let res = text.split('').reverse().join('')
+// console.log(res);
+
+
+//10-masala
+// let text = 'abdulaziz';
+// let char = 'b';
+
+// let index = text.indexOf(char);
+
+// console.log(index);
+
+
+
+
+//12-masala
+// let text = "Abdulaziz";
+
+// let sortedText = text
+//   .split("")
+//   .sort()
+//   .join("");
+
+// console.log(sortedText);
+
+
+//16-masala
+// let text = "Hello-world-how-are-you";
+// let res = text.split(' ').join('-');
+
+// console.log(res);
+
+
+//17-masala
+// let text='hello'
+// let res = text.toUpperCase()
+// console.log(res);
+
+//18-masala
+// let text = 'hello'
+// let res = text.split(' ')
+// console.log(res);
+
+
+//19-masala
+// let text = ' test   '
+// let res = text.trim()
+// console.log(res);
+
+
+
+//Massivga oid
+//1-masala
+// let arr = [
+//     {name:'Ali',age:20},
+//     {name:'Bobur',age:26},
+//     {name:'Salim',age:30}
+// ]
+// let sum = arr[0]
+// arr.forEach(function(value) {
+//     if (sum.age<value.age) {
+//         sum=value
+//     }
+// })
+// console.log(sum);
+
+//2-masala
+// let arr = [
+//   { name: "Ali", age: 20 },
+//   { name: "Salim", age: 30 },
+//   { name: "Bobur", age: 26 },
+// ];
+// let res = arr.sort(function(a, b) {
+//   return a.age - b.age;
+// });
+// console.log(res);
+
+//3-masala
+// let arr = [
+//   { num: 1 },
+//   { num: 2 },
+//   { num: 3 },
+//   { num: 4 },
+//   { num: 5 },
+//   { num: 6 },
+// ];
+// let sum = 0
+// for (let i = 0; i < arr.length; i++) {
+//     sum+=arr[i].num
+// }
+// console.log(sum);
+
+
+
+//4-masala
+// let arr = [
+//   { name: "Ali", age: 20 },
+//   { name: "Salim", age: 30 },
+//   { name: "Bobur", age: 26 },
+// ];
+// let res = arr.filter(function (value) {
+
+//     return value.age >18
+// })
+// console.log(res);
+
+
+//6-masala
+// const arr = [
+//   { firstName: "ABdulaziz", lastName: "Abdurahimov" },
+//   { firstName: "Ahliddin", lastName: "Boqijonov'" },
+// ];
+
+// arr.forEach((value) => {
+//   value.fullName = value.firstName + " " + value.lastName;
+// });
+
+// console.log(arr);
+
+
+
+//7-masala
+// const arr = [
+//   { firstName: "dfg", lastName: "fsgdhf" },
+//   { firstName: "gdhfjgh", lastName: "fsgd" },
+// ];
+
+// const res = arr.find((value) => value.firstName === "dfg");
+
+// if (res) {
+//   res.lastName = "new";
+// }
+
+// console.log(arr);
+
+
+//8-masala
+// let arr = [
+//     {
+//         firsName: 'Abdulaziz',
+//         lastName: 'Abdurahimov',
+//         id:1
+//     },
+//     {
+//         firsName: 'Ahliddin',
+//         lastName: 'Boqijonov',
+//         id:2
+//     }
+// ]
+// let kalit = 1;
+// arr = arr.filter(item => item.id !== kalit);
+
+// console.log(arr);
+
+//9-masala  
+// let arr = [
+//     {
+//         firsName: 'Abdulaziz',
+//         lastName: 'Abdurahimov',
+//         id:1
+//     },
+//     {
+//         firsName: 'Ahliddin',
+//         lastName: 'Boqijonov',
+//         id:2
+//     }
+// ]
+
+
+
+  
+
